1d3709b1f038d2e14962f4879ad1fc7d84a54970
Joe Johnston <joe@simple10.com>
2015-02-23 12:04:03 -0800

    Fix error when NODE_PATH is not defined

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Make stacker-globals/lib files globally available to all plugins via require('module')
 
 // Only allow for one stacker-globals in the path to ensure there's only one copy of each lib
-if (process.env.NODE_PATH.indexOf('stacker-globals') < 0) {
+if (!process.env.NODE_PATH || process.env.NODE_PATH.indexOf('stacker-globals') < 0) {
   var path = require('path');
   process.env.NODE_PATH = path.resolve(__dirname, './lib') + path.delimiter + process.env.NODE_PATH;
   require('module').Module._initPaths();  // Hack
